1fa61594757773a9dce6ecf5fc8a7f732e551a5b
mou-lee <mouliofficial2811@gmail.com>
2026-05-28 18:24:19 +0530

    UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,27 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <h1>Intranet AI Agent</h1>
+    <div className="app">
+      {/* Background Glow */}
+      <div className="glow glow1"></div>
+      <div className="glow glow2"></div>
 
-      <UploadBox />
+      <div className="container">
+        <div className="hero-section">
+          <h1>Intranet AI Agent</h1>
 
-      <ChatBox />
+          <p>
+            AI-powered assistant for company documents,
+            employee support, announcements, and internal knowledge.
+          </p>
+        </div>
+
+        <div className="main-grid">
+          <UploadBox />
+
+          <ChatBox />
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,19 +1,29 @@
+import "../styles/Message.css";
+
 function Message({ role, content, sources }) {
   return (
-    <div className={`message ${role}`}>
-      <p>{content}</p>
-
-      {/* {sources && sources.length > 0 && (
-        <div className="sources">
-          <strong>Sources:</strong>
-
-          <ul>
-            {sources.map((source, index) => (
-              <li key={index}>{source}</li>
-            ))}
-          </ul>
+    <div className={`message-row ${role}`}>
+      {role === "assistant" && (
+        <div className="avatar">
+          J
         </div>
-      )} */}
+      )}
+
+      <div className={`message ${role}`}>
+        <p>{content}</p>
+
+        {/* {sources && sources.length > 0 && (
+          <div className="sources">
+            <strong>Sources:</strong>
+
+            <ul>
+              {sources.map((source, index) => (
+                <li key={index}>{source}</li>
+              ))}
+            </ul>
+          </div>
+        )} */}
+      </div>
     </div>
   );
 }
